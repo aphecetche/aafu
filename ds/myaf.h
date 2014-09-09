a9a09f2f0b33eff5f7fe0c2868c629352a7bfa17
laphecet <laurent.aphecetche@subatech.in2p3.fr>
2014-09-09 12:06:28 +0200

    Finally get the Makefile right

---
+++ ds/myaf.h
@@ -0,0 +1,14 @@
+//
+//  myaf.h
+//  aafu
+//
+//  Created by Laurent Aphecetche on 09/09/2014.
+//  Copyright (c) 2014 CNRS. All rights reserved.
+//
+
+#ifndef aafu_myaf_h
+#define aafu_myaf_h
+
+
+
+#endif
